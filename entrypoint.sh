5e65078d03e480e9da93b94c63c3b043c89ccd04
amolinado <31352466+amolinado@users.noreply.github.com>
2017-08-29 16:23:27 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa && \
+ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa && \
+ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa && \
+ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519 && \
+/usr/sbin/sshd 
+
+# Wetty
+node wetty/app.js -p 3000 &
+
+# Default exec
+EXEC=${EXEC:-xmrig --help}
+
+# Exec ARGS or EXEC
+exec ${@:-$EXEC}
