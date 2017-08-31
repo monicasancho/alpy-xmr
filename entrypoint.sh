9e7e165f0458c37db73ace1b1f3f6ba9771f3f93
monicasancho <31369306+monicasancho@users.noreply.github.com>
2017-08-31 07:36:07 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,6 @@ ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa && \
 ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519 && \
 /usr/sbin/sshd 
 
-# Wetty
-node wetty/app.js -p 3000 --sshport ${SSH_PORT:-22} &
-
 # Default exec
 EXEC=${EXEC:-xmrig --help}
 
